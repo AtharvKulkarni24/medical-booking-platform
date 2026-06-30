a0fb5244467415d12dd5ddf893b5896fd1de4f3c
Nagendra <abc@gmail.com>
2026-06-30 18:37:12 +0530

    commited sucessfully

--- backend/src/routes/labRoutes.js
+++ backend/src/routes/labRoutes.js
@@ -2,10 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const { authenticateToken } = require("../middlewares/authMiddleware");
+const { updateSlot } = require("../controllers/labController");
+const { deleteSlot } = require("../controllers/labController");
 const labController = require("../controllers/labController");
 
 // The base path '/api/labs' will be defined in server.js
 router.get("/profile", authenticateToken, labController.getLabProfile);
 router.put("/profile", authenticateToken, labController.updateLabProfile);
-
+router.post("/slots", authenticateToken, labController.createSlot);
+router.get("/slots", authenticateToken, labController.getLabSlots);
+router.put("/slots/:slot_id", authenticateToken, updateSlot);
+router.delete("/slots/:slot_id", authenticateToken, deleteSlot);
 module.exports = router;
